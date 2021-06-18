45cbe293706e0ca51163cb2400514b1f51dd7bd5
Alvis Ng <alvis.bpng@gmail.com>
2021-06-18 22:09:03 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'next',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'react/jsx-props-no-spreading': ['error', { custom: 'ignore' }],
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
